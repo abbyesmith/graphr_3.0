3731d0c7c56d133241373b0e0d772212af8f8531
Abby <abby.e.smith.85@gmail.com>
2023-04-28 12:46:41 -0600

    Responsive graph, working form, LOBF calculated in console

--- client/pages/components/linear_graph.js
+++ client/pages/components/linear_graph.js
@@ -1,18 +1,57 @@
 import React from "react";
-import {Bar} from 'react-chartjs-2'
+import { Scatter } from 'react-chartjs-2'
 import {Chart as ChartJS} from "chart.js/auto"
 
-export default function BarChart({chartData}) {
-    console.log(chartData)
-    return (
-        <div>
-            <p>test</p>
-            {/* <div style = {{width: 700}}>
-                <Bar data = {chartData} />
-            </div>
-             */}
-            
-        </div>
-    )
 
+export default function ScatterPlot(props){
+
+    const { points } = props
+    const data = {
+        datasets: [{
+            label: 'Points you Plotted',
+            data: points.map((point) => ({x: point.x, y: point.y})),
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            pointRadius: 5,
+            pointHoverRadius: 8,
+                      
+        }]
+    }
+
+    const options = {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+              stepSize: 1,
+              min: -10,
+              max: 10,
+            },
+            grid: {
+              display: true,
+              color: 'grey',
+              borderWidth: 6, //this makes the x-axis bold
+            }
+          },
+          y: {
+            type: 'linear',
+            ticks: {
+              stepSize: 2,
+              min: -10,
+              max: 10,
+            },
+            grid: {
+              display: true,
+              color: 'grey',
+              borderWidth: 9, //the makes the y-axis bold
+            }
+          }
+        }
+      };
+
+    return (
+        <Scatter data={data} options={options} />
+      );
 }
