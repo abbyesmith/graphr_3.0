dbbbaef59a7291f6aad8d4d9b3bd9278c1641739
Abby <abby.e.smith.85@gmail.com>
2023-05-03 15:56:09 -0600

    Copy button win

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,16 +5,49 @@ import html2canvas from 'html2canvas'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// function capture() {
+// 	html2canvas(document.querySelector("#capture")).then(canvas => {
+// 		const resultDiv = document.querySelector("#result");
+//     if (resultDiv) {
+//       resultDiv.appendChild(canvas);
+//     } else {
+//       console.log('Result element not found')
+//     }
+// 	});
+// }
+
+function copyToClipboard(dataUrl) {
+  const blob = new Blob([dataUrl], { type: "image/png; charset=utf-8" });
+  const item = new ClipboardItem({ "image/png": blob });
+
+  navigator.clipboard.write([item]).then(function() {
+    console.log("Copied to clipboard!");
+  }, function(error) {
+    console.error("Failed to copy image to clipboard", error);
+  });
+}
+
 function capture() {
-	html2canvas(document.querySelector("#capture")).then(canvas => {
-		const resultDiv = document.querySelector("#result");
+  html2canvas(document.querySelector("#capture")).then(canvas => {
+    const resultDiv = document.querySelector("#result");
     if (resultDiv) {
       resultDiv.appendChild(canvas);
+      const imgData = canvas.toDataURL('image/png');
+      const uintArray = new Uint8Array(atob(imgData.split(',')[1]).split('').map(char => char.charCodeAt()));
+      const blob = new Blob([uintArray], { type: 'image/png' });
+      navigator.clipboard.write([
+        new ClipboardItem({ [blob.type]: blob })
+      ]).then(() => {
+        console.log('Image copied to clipboard');
+      }).catch(err => {
+        console.error('Failed to copy image to clipboard:', err);
+      });
     } else {
       console.log('Result element not found')
     }
-	});
+  });
 }
+
 
 export default function Home() {
   return (
@@ -25,7 +58,7 @@ export default function Home() {
         <p>This is the content that will be captured</p>
       </div>
       <button onClick = {capture}>Take a screenshot</button>
-      <div id="result">hi!</div>
+      {/* <div id="result">hi!</div> */}
     </div>
   )
 }
